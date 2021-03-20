2223ff46e71528b5905680bc9f15006683f66bab
Joseph Choi <jchoi192@macbook-pro.myfiosgateway.com>
2021-03-20 19:08:22 -0400

    wip: updated js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,51 @@
-alert('JS is working');
-
 // constant variables
+const BASE_URL = 'https://www.balldontlie.io/api/v1/teams';
+
 
 // state variables
+let teamInfo;
 
 // cached element references (jQuery)
+const $teamInfo = $('#teamInfo');
+const $input = $('input[type="text"]')
 
 // event listeners
 
+$('form').on('click', handleGetTeam)
+
 //functions
 
+init();
+
+function init() {
+    getData();
+}
+
+function handleGetTeam(event) {
+    event.preventDefault()
+    userInput = $input.val();
+}
+
 function getData() {
-    $.ajax('https://www.balldontlie.io/api/v1/teams')
-        .then(function (data) {
-            console.log(data)
-        }, function (error) {
-            console.log(error)
-        });
+    $.ajax(BASE_URL).then(function (data) {
+        console.log(data);
+        teamInfo = data;
+        render();
+    }, function (error) {
+        console.log(error);
+    });
+}
+
+function render() {
+    const html = teamInfo.forEach(function(info) {
+        console.log(info);
+        return            
+            `
+            <h1>${info.data.full_name}</h1>
+            <p>${info.data.abbreviation}</p>
+            <p>${info.data.conference}</p>
+            <p>${info.data.division}</p>
+         `;
+    });
+    console.log(html);
 }
